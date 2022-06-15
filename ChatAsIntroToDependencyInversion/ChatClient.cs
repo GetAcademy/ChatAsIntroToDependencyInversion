0318da7d889b4137868b362e8698cbced09d0966
GET Academy <terje@getacademy.no>
2022-06-15 11:26:08 +0200

    Initial commit

---
+++ ChatAsIntroToDependencyInversion/ChatClient.cs
@@ -0,0 +1,26 @@
+﻿namespace ChatAsIntroToDependencyInversion
+{
+    internal class ChatClient
+    {
+        private string _name;
+        private ChatServer _server;
+
+        public ChatClient(string name, ChatServer server)
+        {
+            _server = server;
+            _name = name;
+            _server.Register(this);
+        }
+
+        public void Say(string message)
+        {
+            _server.Broadcast(this, $"{_name} sier {message}.");
+            ;
+        }
+
+        public void Recieve(string message)
+        {
+            Console.WriteLine($"{_name} mottok: {message}");
+        }
+    }
+}
---
+++ ChatAsIntroToDependencyInversion/ChatServer.cs
@@ -0,0 +1,25 @@
+﻿namespace ChatAsIntroToDependencyInversion
+{
+    internal class ChatServer
+    {
+        List<ChatClient> _clients;
+
+        public ChatServer()
+        {
+            _clients = new List<ChatClient>();
+        }
+
+        public void Broadcast(ChatClient client, string message)
+        {
+            foreach (var chatClient in _clients.Where(chatClient => chatClient != client))
+            {
+                chatClient.Recieve(message);
+            }
+        }
+
+        public void Register(ChatClient chatClient)
+        {
+            _clients.Add(chatClient);
+        }
+    }
+}
