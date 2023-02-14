8e887c88781129f1f95d79b5dbb4f0ccc163d7d0
GET Academy <terje@getacademy.no>
2023-02-14 09:47:02 +0100

    Update Program.cs

--- ChatAsIntroToDependencyInversion/Program.cs
+++ ChatAsIntroToDependencyInversion/Program.cs
@@ -1,13 +1,4 @@
-﻿/*
- * 1: Lage eksempel ChatClient og ChatServer
- * 2: Vise hvordan gjensidig avhengighet gjør at de to klassene ikke kan deles i ulike prosjekter
- * 3: Dependency Inversion Principle generelt
- * 4: Dependency Inversion Principle for ChatServer og ChatClient
- * 5: Løsningen - et interface!
- * 6: Skrive om koden med interface
- */
-
-using ChatAsIntroToDependencyInversion;
+﻿using ChatAsIntroToDependencyInversion;
 
 var server = new ChatServer();
 
